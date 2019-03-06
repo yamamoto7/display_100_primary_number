07c2ec46c2d553c2a65bd1e42d30bfbc68937e5f
NoharaMasato <masato338914914@gmail.com>
2019-03-06 12:14:20 +0900

    little change

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 int sosuu(int n){
     int result = 0;
     for (int k(2); k < n ;k++){
@@ -13,10 +12,10 @@ int sosuu(int n){
 
 int main(int argc, const char * argv[]) {
     int i(0),n(1);
-    while (i < 100){
+    while (i <= 100){
         if (sosuu(n) != 1){
             i++;
-            cout << n << endl;
+            cout << n << ",";
         }
         n++;
     }
