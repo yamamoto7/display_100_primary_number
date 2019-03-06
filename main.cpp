13284bd54586293ec28e21ca1e31f6d6992cf906
野原将人2 <noharamasato2@noharamasato2noMacBook-Pro.local>
2019-03-06 11:48:15 +0900

    finish

--- main.cpp
+++ main.cpp
@@ -7,9 +7,26 @@
 //
 
 #include <iostream>
+using namespace std;
+int sosuu(int n){
+    int result = 0;
+    for (int k(2); k < n ;k++){
+        if (n % k == 0) {
+            result = 1;
+        }
+    }
+    return result;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int i(0),n(1);
+    while (i < 100){
+        if (sosuu(n) != 1){
+            i++;
+            cout << n << endl;
+        }
+        n++;
+    }
     return 0;
 }
