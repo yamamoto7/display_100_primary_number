33f27bc702ea92fdb51296afbde0fb599d7a06be
野原将人2 <noharamasato2@noharamasato2noMacBook-Pro.local>
2019-03-06 11:48:43 +0900

    fix comment

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
-//
-//  main.cpp
-//  sosuu
-//
-//  Created by 野原将人2 on 2019/03/06.
-//  Copyright © 2019 野原将人2. All rights reserved.
-//
-
 #include <iostream>
 using namespace std;
+
 int sosuu(int n){
     int result = 0;
     for (int k(2); k < n ;k++){
@@ -19,7 +12,6 @@ int sosuu(int n){
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     int i(0),n(1);
     while (i < 100){
         if (sosuu(n) != 1){
