a16844b0a53fb7b3187e1ed4f5093917748e2b2b
野原将人2 <noharamasato2@noharamasato2noMacBook-Pro.local>
2019-03-06 11:37:47 +0900

    create file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sosuu
+//
+//  Created by 野原将人2 on 2019/03/06.
+//  Copyright © 2019 野原将人2. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
